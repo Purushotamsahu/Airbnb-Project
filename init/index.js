389a13cc6792bd4ecebf73c9e3c8fffb7494de1c
Purushotam sahu <purushotam176@gmail.com>
2026-05-12 19:13:51 +0530

    updated project

--- init/index.js
+++ init/index.js
@@ -1,27 +1,6 @@
-// const mongoose = require('mongoose');
-// const initData = require('./data.js');
-// const Listing = require('../models/listing.js')
-
-// const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
-// main().then(() => {
-//     console.log("Connected to DB");
-// })
-// .catch((err) => {
-//     console.log(err);
-// })
-
-// async function main(){
-//     await mongoose.connect(MONGO_URL);
-//     console.log("Connected to DB");
-//     await initDB();
-// }
-
-// const initDB = async () => {
-//     await Listing.deleteMany({});
-//     initData.data = initData.data.map((obj) => ({ ...obj, owner: "69fc22e24e7a3f0e8ce771af"}));
-//     await Listing.insertMany(initData.data);
-//     console.log("Data was intialized");
-// }
+if(process.env.NODE_ENV != "production"){
+    require('dotenv').config({ path: '../.env' });
+}
 
 
 const mongoose = require("mongoose");
@@ -29,21 +8,21 @@ const initData = require("./data.js");
 const Listing = require("../models/listing.js");
 const axios = require("axios");
 
-const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+const dbUrl = process.env.ATLASDB_URL;
 
 main()
   .then(() => console.log("Connected to DB"))
   .catch((err) => console.log(err));
 
 async function main() {
-  await mongoose.connect(MONGO_URL);
+  await mongoose.connect(dbUrl);
   await initDB();
 }
 
-// ⏳ delay helper (prevents rate limit)
+//delay helper (prevents rate limit)
 const sleep = (ms) => new Promise((res) => setTimeout(res, ms));
 
-// 🔁 retry-safe geocoding function
+//retry-safe geocoding function
 const fetchCoords = async (query, retries = 3) => {
   try {
     return await axios.get(
@@ -112,7 +91,7 @@ const initDB = async () => {
       console.log(`Error for ${obj.location}:`, err.message);
     }
 
-    // ⛔ IMPORTANT: respect API limits
+    //  IMPORTANT: respect API limits
     await sleep(1200);
   }
 
